1f333a97ed0720201a71c642a566eb8e2072dae3
Neelmani <75542781+neelmani15@users.noreply.github.com>
2023-11-11 10:51:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,13 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin:["https://gmail-clone-seven-nu.vercel.app/"],
+    methods:["POST","GET"],
+    credentials:true
+  }
+));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use('/',routes);
