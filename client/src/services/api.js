70b5f5214d0b1142d7a69d262b7d0268f12e4b66
Neelmani <75542781+neelmani15@users.noreply.github.com>
2023-11-11 11:20:23 +0530

    Update api.js with vercel

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const API_URL = 'https://gmail-clone-eta-two.vercel.app/';
 
+axios.defaults.withCredentials = true;
 const API_GMAIL=async(urlObject,payload,type)=>{
     return await axios({
         method:urlObject.method,
